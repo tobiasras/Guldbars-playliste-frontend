b2fceaa2eb55ae6ac3b36609730a06e0b218e485
Magnus <magn902m@stud.kea.dk>
2023-04-21 10:50:42 +0200

    search for tracks in add song page

--- src/component/Track.jsx
+++ src/component/Track.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Track = (props) => {
     return (
         <>
-            <article id={`stackId${props.id}`} className="flex gap-4 items-center">
+            <article id={`stackId${props.id}`} className="flex gap-4 items-center justify-between w-full">
                 <img src={props.images[0].url} alt={props.name} className="w-16 min-w-[3rem] rounded-md" />
                 <div>
                     <h4 className="text-xl">{props.name}</h4>
--- src/pages/FrontpagePage.jsx
+++ src/pages/FrontpagePage.jsx
@@ -3,19 +3,20 @@ import { Queue } from "../component/Queue";
 
 const FrontpagePage = () => {
 
-
     return (
-        <div className="w-screen h-full overflow-x-hidden overflow-y-scroll grid grid-cols-[1fr_minmax(0,1400px)_1fr] items-center place-content-center bg-neutral-800 text-white">
-            <main className="flex justify-center col-[2/3]">
+        <div className="w-screen h-full overflow-x-hidden overflow-y-scroll grid grid-cols-[1fr_minmax(0,1000px)_1fr] items-center place-content-center bg-neutral-800 text-white">
+            <main className="flex flex-col col-[2/3]">
                 <section>
+                    <h1>Current track playing</h1>
+                    <p>Working</p>
                 </section>
                 <section className="flex flex-col">          
-                    <h1 className="text-center text-4xl mb-4">Current queue</h1>
+                    <h2 className="text-center text-4xl mb-4">Current queue</h2>
                     <Queue/>
                 </section>   
 
-                <Navbar/>
             </main>
+            <Navbar/>
         </div>
     )
 }

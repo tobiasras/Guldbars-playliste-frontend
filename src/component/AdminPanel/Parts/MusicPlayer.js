7f6ca46422a4c85a6e776c2cdb595b276a45fdfc
tobiasras <tobiasras.job@gmail.com>
2023-04-24 12:20:38 +0200

    fixed music player

--- src/component/AdminPanel/Parts/MusicPlayer.js
+++ src/component/AdminPanel/Parts/MusicPlayer.js
@@ -57,7 +57,7 @@ const MusicPlayer = () => {
 
     return (
         <div className="w-full bg-neutral-700 rounded flex justify-around p-3">
-            <button type="button" onClick={next}
+            <button type="button" onClick={previous}
                     className="text-gray-500 bg-neutral-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg pt-2 sm:w-auto px-5  text-center ">
                 <span className="material-symbols-outlined p-0 m-0">
                     first_page
@@ -83,7 +83,7 @@ const MusicPlayer = () => {
                 </button>
             }
 
-            <button type="button" onClick={previous}
+            <button type="button" onClick={next}
                     className="text-gray-500 bg-neutral-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg pt-2 sm:w-auto px-5  text-center ">
                 <span className="material-symbols-outlined p-0  m-0">
                      last_page

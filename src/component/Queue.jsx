0ec229b40451f54291e0b723eb3a27132f7f2815
Magnus <magn902m@stud.kea.dk>
2023-04-24 17:46:38 +0200

    outcomment html for voting

--- src/component/Queue.jsx
+++ src/component/Queue.jsx
@@ -23,13 +23,13 @@ export const Queue = () => {
                 {queue !== "" ? queue.map((trackInfo) => (
                     <li className="flex items-center gap-4 bg-neutral-600 p-4 rounded-md">
                         <Track key={trackInfo.id} {...trackInfo} />
-                        <div className="flex items-center gap-2">                        
+                        {/* <div className="flex items-center gap-2">                        
                             <p className="text-center font-black">12</p>
                             <div className="flex flex-col gap-2">                        
                                 <button className="py-[0.4rem] px-3 hover:bg-neutral-800 rounded-full">&uarr;</button>
                                 <button className="py-[0.4rem] px-3 hover:bg-neutral-800 rounded-full">&darr;</button>
                             </div>
-                        </div>
+                        </div> */}
                     </li>
                 )) : "Chould not get track"}
                 </ul>) : 

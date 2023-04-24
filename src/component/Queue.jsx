01f5bdbb69c7d4556ab4df6c7792c7244a1602d7
Magnus <magn902m@stud.kea.dk>
2023-04-24 17:44:00 +0200

    fixed error so user dont need to login, and add switch case for result status

--- src/component/Queue.jsx
+++ src/component/Queue.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import {Link} from "react-router-dom";
 import {Track} from "../component/Track";
 
 export const Queue = () => {
@@ -18,11 +19,16 @@ export const Queue = () => {
     
     return (
         <>
-            <ul className="flex flex-col gap-8">
+            {queue.message !== "Bad or expired token" ? (
+                <ul className="flex flex-col gap-8">
                 {queue !== "" ? queue.map((trackInfo) => (
                     <Track key={trackInfo.id} {...trackInfo} />
                 )) : "Chould not get track"}
-            </ul>
+                </ul>) : 
+                (<ul className="">
+                    You need to <Link to="/admin" className="underline">login</Link>
+                </ul>)  
+            }     
         </>
     )
 }

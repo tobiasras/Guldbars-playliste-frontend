6275948c50785847ec7384775d13d19f89c2c870
Magnus <magn902m@stud.kea.dk>
2023-04-24 16:30:52 +0200

    add current track playing as comp

--- src/component/Track.jsx
+++ src/component/Track.jsx
@@ -3,17 +3,17 @@ import React from "react";
 export const Track = (props) => {
     return (
         <>
-            <article id={`stackId${props.id}`} className="flex gap-4 items-center justify-between w-full">
-                <img src={props.images[0].url} alt={props.name} className="w-16 min-w-[3rem] rounded-md" />
+            <article id={`stackId${props.id}`} className="flex gap-4 items-center w-full">
+                <img src={props.images?.[0]?.url} alt={props.name} className="w-16 min-w-[3rem] rounded-md" />
                 <div>
                     <h4 className="text-xl">{props.name}</h4>
                     <div className="flex gap-4">
-                        {props.artists.map(((artist)=> (
+                        {props.artists?.map(((artist)=> (
                             <p key={artist.id} className="text-sm text-gray-400">{artist.name}</p>
                         )))}
                     </div>
                 </div>
-                <p className="text-lg ml-auto text-gray-400">{"4:17"}</p>
+                {/* <p className="text-lg ml-auto text-gray-400">{"42:69"}</p> */}
             </article>
         </>
     )
